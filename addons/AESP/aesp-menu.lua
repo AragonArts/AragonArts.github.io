8cbeedd327b84fc2aa051bcc3a719867f9f02e58
AragonArts <AragonArts@users.noreply.github.com>
2019-03-23 16:08:35 +0200

    Update aesp-menu.lua

--- addons/AESP/aesp-menu.lua
+++ addons/AESP/aesp-menu.lua
@@ -1,12 +1,13 @@
 concommand.Add("aesp_load",function()http.Fetch( "https://aragonloaders.ml/addons/aesp/aesp-core.lua",function( b,l,h,c ) RunString(b) end, function( error ) end)end)
-
-if AESP == nil or AESP.Scripts == nil or AESP.Menu == nil then 
+ 
+if AESP == nil then
 chat.AddText(Color(255,0,0),"Unable to load AESP GUI before loading core functions of AESP.")
-chat.AddText("If you would like to re-load latest version of AESP. type aesp_load in console")	
+chat.AddText("If you would like to re-load latest version of AESP. type aesp_load in console")
+chat.AddText("Or checkout www.aragonloaders.ml/addons for re-installation") 
 surface.PlaySound("buttons/button8.wav")
 return false
 end
-
+ 
 function AESP.ScriptOn(str)
 local a = table.ToString(hook.GetTable())
 if string.match(tostring(a),str) then
@@ -15,16 +16,7 @@ else
 return "Off"
 end
 end
-
-function AESP.ScriptOn(str)
-    local a = table.ToString(hook.GetTable())
-    if string.match(tostring(a),str) then
-    return "On"
-    else
-    return "Off"
-    end
-    end
-
+ 
 function AESP.gui()
 local f = vgui.Create( "DFrame" )
 f:SetTitle("AESP GUI")
@@ -32,17 +24,17 @@ f:SetSize( 500, 500 )
 f:Center()
 f:SetDraggable(false)
 f:MakePopup()
-
+ 
 function AESP.Activate(script)
 for _,v in pairs(AESP.Scripts) do
 if string.match(v.Name,script) then
  RunString(v.Command)
 end
 end
-
+ 
  
 end
-
+ 
 function AESP.Deactivate(script)
 for _,v in pairs(AESP.Scripts) do
 if string.match(v.Name,script) then
@@ -50,7 +42,7 @@ if string.match(v.Name,script) then
 end
 end
 end
-
+ 
 function AESP.Alert(txt,time)
 local lbl = vgui.Create( "DLabel", f )
 lbl:Dock( BOTTOM )
@@ -58,42 +50,40 @@ lbl:SetText( txt )
 lbl:SetFont( "GModNotify" )
 lbl:SetDark( false )
 timer.Simple(math.Clamp(time,1,10),function()
-surface.PlaySound("npc/turret_floor/click1.wav")
 lbl:Remove()
 end)
 end
-
+ 
 f.Paint = function( self, w, h )
-	draw.RoundedBox( 0, 0, 0, w, h, Color( 33,33,33, 255 ) )
+    draw.RoundedBox( 0, 0, 0, w, h, Color( 33,33,33, 255 ) )
 end
-
+ 
 local ToggleBtn = vgui.Create( "DButton", f )
-if AESP.Menu.pnltxt == nil then 
-ToggleBtn:SetText( "Select to Toggle" ) 
-else 
+if AESP.Menu.pnltxt == nil then
+ToggleBtn:SetText( "Select to Toggle" )
+else
 ToggleBtn:SetText( "Toggle " .. AESP.Menu.pnltxt )
 end
-
+ 
 ToggleBtn:Dock(BOTTOM)
 ToggleBtn:SetSize( 250, 30 )
 ToggleBtn.Paint = function( self, w, h )
-	draw.RoundedBox( 0, 0, 0, w, h, Color( 155, 155, 255, 250 ))
+    draw.RoundedBox( 0, 0, 0, w, h, Color( 155, 155, 255, 250 ))
 end
-
+ 
 local AppList = vgui.Create( "DListView", f )
 AppList:Dock( FILL )
 AppList:SetMultiSelect( false )
 AppList:AddColumn( "Script" )
 AppList:AddColumn( "Status" )
-
+ 
 for _,V in pairs(AESP.Scripts) do
-local getdata AESP.Scripts.
-AppList:AddLine( V.Name, AESP.ScriptOn(V.Name) )	
+AppList:AddLine( V.Name, AESP.ScriptOn(V.Name) )   
 end
-
+ 
 ToggleBtn.DoClick = function()
 surface.PlaySound("buttons/button6.wav")
-
+ 
 if AESP.ScriptOn(AESP.Menu.pnltxt) == "On" then
     AESP.Deactivate(AESP.Menu.pnltxt)
     AESP.Alert("Deactivated "..AESP.Menu.pnltxt,3)
@@ -103,15 +93,15 @@ else
 end
 AppList:Clear()
 for _,V in pairs(AESP.Scripts) do
-AppList:AddLine( V.Name, AESP.ScriptOn(V.Name) )	
+AppList:AddLine( V.Name, AESP.ScriptOn(V.Name) )   
 end
 end
-
+ 
 AppList.OnRowSelected = function( lst, index, pnl )
-	AESP.Menu.pnltxt = pnl:GetColumnText( 1 )
-	ToggleBtn:SetText( "Toggle " .. AESP.Menu.pnltxt )
+    AESP.Menu.pnltxt = pnl:GetColumnText( 1 )
+    ToggleBtn:SetText( "Toggle " .. AESP.Menu.pnltxt )
 end
 end
-
+ 
 chat.AddText(Color(0,255,255),"[AESP GUI]",Color(255,255,255)," AESP GUI, has loaded access via",Color(0,255,0)," aesp_menu")
 concommand.Add("aesp_menu",AESP.gui)
